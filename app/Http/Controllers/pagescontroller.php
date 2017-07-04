0d5c135aa19161b63a41e8be771f41b2c0aeddcc
Dennis Lazar <dla@qosmotec.com>
2017-07-04 16:03:50 +0200

    first commit

---
+++ app/Http/Controllers/pagescontroller.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PagesController extends Controller {
+
+	public function getIndex() {
+		return view('pages.login');
+	}
+	public function getAbout() {
+		$first = 'our';
+		$last = 'school';
+		$fullname = $first . " " . $last;
+		$tel = '+49 241 93 20 95';
+
+
+		$data = [];
+		$data['fullname'] = $fullname;
+		$data['tel'] = $tel;
+		return view('pages.about')->withData($data);
+	}
+	public function getContact() {
+		return view('pages.contact');
+	}
+	public function getHome() {
+		return view('pages.home');
+	}
+
+
+}
